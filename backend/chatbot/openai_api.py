e85b61dd590144b37f95ec68fa12a7ef3e07b129
Sebastian Furn <sfurn@kth.se>
2023-07-28 04:02:26 +0200

    No agent anymore

--- backend/chatbot/openai_api.py
+++ backend/chatbot/openai_api.py
@@ -12,76 +12,66 @@ openai.api_key_path = "/home/anon/.secret"
 llm3 = "gpt-3.5-turbo"
 llm4 = "gpt-4-0613"
 
-system_template = """
-Here are the actions you have access to along with a description of each one so you know when to use them:
+functions=[
 {
-    {
-        Action: get_locations,
-        Description: Used whenever the question involves location of any sort,
-        {
-            Argument: locations,
-            Description: A comma-separated list of the locations the user is asking for,
-        },
-        Required arguments: [location],
+"name": "get_locations",
+"description": "Provides directions to multiple locations",
+"parameters": {
+    "type": "object",
+    "properties": {
+        "locations": {
+            "type": "array",
+            "description": "The locations to provide directions for",
+            "items": {
+                "type": "string"
+            }
+        }
     },
-    {
-        Action: get_schedule,
-        Description: Returns the schedule for the specified timespan,
-        {
-            Argument: timespan,
-            Description: The timespan the user want to know the schedule of,
-        },
-        Required arguments: [timespan],
+    "required": ["locations"]
+}},
+{
+"name": "get_schedule",
+"description": "Provides the schedule within a time span",
+"parameters": {
+    "type": "object",
+    "properties": {
+        "time_span": {
+            "type": "string",
+            "description": "The time span to retrieve the schedule from",
+        }
     },
-}
-
-I will give you a question and I want you to go about answering my question in a very specific way following this system:
-Question: My question to you
-Thought: You will think about the question and think step by step on how to find the information needed to answer the question based on the actions you have available
-Action: You will now decide which action to take next based on your plan and provide arguments within square-brackets
-Observation: The result of the action
-(This Thought, Action, Observation can repeat 6 times and needs to happen at least once. If none of the available actions seem relevant I want your answer to be: 'Couldn't find relevant action to your question')
-Thought: I have the answer
-Final Answer: The final answer to the original question
+    "required": ["time_span"]
+}}]
 
 
-EXAMPLES (Just to show the thought pattern, all information here is fabricated):
-Question: Where can can I find Alba Nova and B25?
-Thought: In order to provide the user the location of Alba Nova and B25 we have to call the function get_locations with correct arguments
-Action: get_locations[Alba Nova,B25]
-Observation: 
-[Alba Nova](https://www.google.com/maps/place/59.35393306147455,18.057654294803914)
-[B25](https://www.google.com/maps/place/59.35151479446071,18.068663853571927) (Floor 3)
-Thought: I have the answer
-Final Answer: 
-[Alba Nova](https://www.google.com/maps/place/59.35393306147455,18.057654294803914)
-[B25](https://www.google.com/maps/place/59.35151479446071,18.068663853571927) (Floor 3)
-Question: What is happening on 13th October?
-Thought: In order to tell the user what happens on 13th October we first need to get the schedule for that day by calling get_schedule. We can then use that information answer the question
-Action: get_schedule[13th October]
-Observation:
-date,start,end,activity,course,rooms
-2023-10-13,08:00,10:00,Partial Exam/Quiz,SG1113,"V23,V32-33,W25,W37"
-2023-10-13,13:00,15:00,Exercise,SF1683,"V11,V21-22"
-2023-10-13,15:00,17:00,Math help session,SI1146,FB52
-Thought: I have the answer
-Final Answer: 
-Schedule:
+system_template = f"""
+You are an assistant at the university of KTH and your task is to help students navigate campus and interpret their schedule.
+Your abilities are that you can read their schedule and provide them with directions to different buildings, rooms and parks around campus.
+"""
+"""
+user_question = "Where can I find E, V and B12?"
 
-2023-10-13:
-08:00-10:00 - Partial Exam/Quiz
-   Course: SG1113
-   Rooms: V23, V32-33, W25, W37
+messages = []
+messages.append({"role": "system", "content": system_template})
+messages.append({"role": "user", "content": user_question})
 
-13:00-15:00 - Exercise
-   Course: SF1683
-   Rooms: V11, V21-22
+response = openai.ChatCompletion.create(
+    model=llm4,
+    messages=messages,
+    functions=functions,
+    function_call="auto"
+).choices[0].message
 
-15:00-17:00 - Math help session
-   Course: SI1146
-   Room: FB52
+print(response)
+function_name = response['function_call']['name']
+function_arguments = response.to_dict()['function_call']['arguments']
+function_arguments = json.loads(function_arguments)['locations']
 
-You are an assistant for KTH and your mission is to help students interpret their schedule and navigate campus.
+print(function_name)
+print(function_arguments)
+function_name = eval(function_name)
+print(function_name(function_arguments))
+exit()
 """
 
 # Test if user asked for specific place with regex
@@ -139,7 +129,6 @@ def get_location(location):
 
 # The callable functions
 def get_locations(locations):
-    locations = locations.split(",")
     ans = []
     for location in locations:
         ans.append(get_location(location))
@@ -171,35 +160,6 @@ def format_date(timespan):
     return date_ans
 
 
-def calculate_week_number(week_string):
-    # Retrieve current week number in case week_string is relative
-    current_week = datetime.date(datetime.year, datetime.data.month, datetime.data.day).isocalendar().week
-    prompt = f"""
-    The current week is {current_week} and I want you to tell me the week number that the user refers to, and make sure that your answer is an integer.
-
-    Here is an example so you understand better:
-    EXAMPLE CURRENT WEEK: 10
-    EXAMPLE INPUT: next week
-    EXAMPLE OUTPUT: 11
-
-    INPUT: {week_string}
-    OUTPUT:
-    """
-    response = openai.ChatCompletion.create(
-        model=llm4,
-        messages=[
-            {"role": "user", "content": prompt},
-        ],
-    )
-    week_nr = response["choices"][0]["message"]
-    try:
-        week_nr = int(week_nr)
-    except:
-        print("AI did not return week as a number")
-        exit()
-
-    return week_nr
-
 def make_location_list(rooms):
     df = pd.read_csv("/home/anon/Code/Other/test/chatbot/Data/regex_places.csv", delimiter='|')
     ret = []
@@ -218,15 +178,32 @@ def make_location_list(rooms):
 
     return ret
 
-def get_schedule(timespan, grade):
+def get_schedule(arr):
+    timespan, grade = arr
     start, end = format_date(timespan).split(',')
     start = start.strip()
     end = end.strip()
     activities = []
+
     start_date = datetime.datetime.strptime(start, "%Y-%m-%d")
     end_date = datetime.datetime.strptime(end, "%Y-%m-%d")
+
     if (end_date - start_date).days >= 20:
         return "Your time span is too large. You have to select a time span smaller than 20 days"
+
+    ans = ""
+    with open(f"./chatbot/Data/tefy_schedule_s{grade}.csv") as file:
+        print(grade)
+        for line in file.readlines():
+            try:
+                template_date = datetime.datetime.strptime(line[0:10], "%Y-%m-%d")
+            except:
+                continue
+            if "Begin" in line or start_date <= template_date <= end_date:
+                ans += line+'\n'
+    
+    return ans
+
     df = pd.read_csv(f"/home/anon/Code/Other/test/chatbot/Data/tefy_schedule_s{grade}.csv", delimiter=',')
     for i in df.iterrows():
         template_date = datetime.datetime.strptime(i[1][0], "%Y-%m-%d")
@@ -245,28 +222,6 @@ def get_microwaves():
     # Read data from microwave file and just return it pretty much
     pass
 
- 
-# User interaction
-def call_function(message, grade):
-    thought_end = message.index("Action")
-    function_name_start = thought_end + len("Action: ")
-    function_name_end = message.index("[")
-    function_args_start = function_name_end + 1
-    function_args_end = message.index("]")
-    function_name = message[function_name_start:function_name_end]
-    function_args = message[function_args_start:function_args_end]
-
-    callable_function = eval(function_name)
-    if function_name == "get_schedule":
-        function_response = callable_function(function_args, grade)
-    else:
-        function_response = callable_function(function_args)
-
-    prev_content = message[:function_args_end+2]
-    prev_content += f"\nObservation: {function_response}"
-
-    return prev_content
-    
 def get_role(role):
     if role == 1:
         return "user"
@@ -274,33 +229,43 @@ def get_role(role):
         return "assistant"
 
 def main(user_id, grade):
-    history = [
+    messages = [
         {"role": "system", "content": system_template},
     ]
-    safe_net = 5
-    messages = TextBlock.objects.filter(user=user_id).order_by('created_at')
 
-    message_len = len(messages)
-    for i in range(message_len-1):
-        print(f"is_user: {get_role(messages[i].is_user)}, content: {messages[i].text}")
-        history.append({"role": get_role(messages[i].is_user), "content": messages[i].text})
-    history.append({"role": get_role(messages[message_len-1].is_user), "content": "Question: "+messages[message_len-1].text})
+    # Gets the chatlog for the specified user with and id of user_id
+    text_blocks = TextBlock.objects.filter(user=user_id).order_by('created_at')
 
-    while(safe_net >= 0):
-        safe_net -= 1
-        response = openai.ChatCompletion.create(
-            model=llm4,
-            messages=history,
-        )
-        message = response["choices"][0]["message"]["content"]
-        if "Final Answer" in message:
-            start_end = message.index("Final Answer") + len("Final Answer: ")
-            final_answer = message[start_end:]
-            history.append({"role": "assistant", "content": final_answer})
-            print(f"Final Answer: {final_answer}")
-            return final_answer
-        message = response["choices"][0]["message"]["content"]
-        prev_message = call_function(message, grade)
-        print(prev_message)
-        history.append({"role": "assistant", "content": prev_message})
+    # Adds the chatlog to 'messages'
+    for text_block in text_blocks:
+        role = get_role(text_block.is_user)
+        content = text_block.text
+        messages.append({"role": role, "content": content})
+
+    response = openai.ChatCompletion.create(
+        model=llm4,
+        messages=messages,
+        functions=functions,
+        function_call="auto"
+    ).choices[0].message
+    response_text = response['content']
+
+    if response_text != None:
+        # Didn't return a function
+        return response_text
+
+    # Did return a function
+    # Parses function name and arguements
+    function_name = response['function_call']['name']
+    function_arguments = response.to_dict()['function_call']['arguments']
+    function_arguments = json.loads(function_arguments)
+
+    if function_name == 'get_locations':
+        function_arguments = function_arguments['locations']
+    else:
+        function_arguments = [function_arguments['time_span'], grade]
+
+    function_name = eval(function_name)
+    func_response = function_name(function_arguments)
+    return func_response
 
---
+++ backend/chatbot/templates.py
@@ -0,0 +1,59 @@
+tools = """
+{
+    {
+        Action: get_locations,
+        Description: Used whenever the question involves location of any sort,
+        {
+            Argument: locations,
+            Description: A comma-separated list of the locations the user is asking for,
+        },
+        Required arguments: [location],
+    },
+    {
+        Action: get_schedule,
+        Description: Returns the schedule for the specified timespan,
+        {
+            Argument: timespan,
+            Description: The timespan the user want to know the schedule of,
+        },
+        Required arguments: [timespan],
+    },
+}
+"""
+
+examples = """
+Question: Where can can I find Alba Nova and B25?
+Thought: In order to provide the user the location of Alba Nova and B25 we have to call the function get_locations with correct arguments
+Action: get_locations[Alba Nova,B25]
+Observation: 
+[Alba Nova](https://www.google.com/maps/place/59.35393306147455,18.057654294803914)
+[B25](https://www.google.com/maps/place/59.35151479446071,18.068663853571927) (Floor 3)
+Thought: I have the answer
+Final Answer: 
+[Alba Nova](https://www.google.com/maps/place/59.35393306147455,18.057654294803914)
+[B25](https://www.google.com/maps/place/59.35151479446071,18.068663853571927) (Floor 3)
+Question: What is happening on 13th October?
+Thought: In order to tell the user what happens on 13th October we first need to get the schedule for that day by calling get_schedule. We can then use that information answer the question
+Action: get_schedule[13th October]
+Observation:
+date,start,end,activity,course,rooms
+2023-10-13,08:00,10:00,Partial Exam/Quiz,SG1113,"V23,V32-33,W25,W37"
+2023-10-13,13:00,15:00,Exercise,SF1683,"V11,V21-22"
+2023-10-13,15:00,17:00,Math help session,SI1146,FB52
+Thought: I have the answer
+Final Answer: 
+Schedule:
+
+2023-10-13:
+08:00-10:00 - Partial Exam/Quiz
+   Course: SG1113
+   Rooms: V23, V32-33, W25, W37
+
+13:00-15:00 - Exercise
+   Course: SF1683
+   Rooms: V11, V21-22
+
+15:00-17:00 - Math help session
+   Course: SI1146
+   Room: FB52
+"""
